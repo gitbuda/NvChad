245096da40cd1a8973ca96e934a8d4df85fce4a0
Marko Budiselic <mbudiselicbuda@gmail.com>
2021-08-28 19:32:31 +0200

    Add custom stuff (make folder, search ignores)

--- init.lua
+++ init.lua
@@ -8,3 +8,7 @@ for _, module in ipairs(init_modules) do
       error("Error loading " .. module .. "\n\n" .. err)
    end
 end
+
+-- TODO(gitbuda): Find a better place for the user config.
+-- Use build as a make directory
+vim.cmd [[ let &makeprg="(cd build && make -j8)" ]]
