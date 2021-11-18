200019c62283c424ac4a5d392683bdf4cc954886
Marko Budiselic <mbudiselicbuda@gmail.com>
2021-11-18 18:37:12 +0100

    Add custom config files

--- init.lua
+++ init.lua
@@ -3,7 +3,3 @@ local ok, err = pcall(require, "core")
 if not ok then
    error("Error loading core" .. "\n\n" .. err)
 end
-
--- TODO(gitbuda): Find a better place for the user config.
--- Use build as a make directory
-vim.cmd [[ let &makeprg="(cd build && make -j8)" ]]
